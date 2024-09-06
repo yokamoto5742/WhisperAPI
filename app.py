85c143f6e8704154c71b1838b078f059597e8300
yokamoto5742-h <yokamoto5742@gmail.com>
2024-09-06 19:48:55 +0900

    アプリの説明

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ import tempfile
 import os
 from groq import Groq
 
-# Groqクライアントの設定
 client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 
 
+def load_markdown_file(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return file.read()
+
+
 def transcribe_audio(file):
-    """ 音声ファイルを文字起こしする """
     try:
         with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio:
             temp_audio.write(file.getvalue())
@@ -29,6 +32,19 @@ def transcribe_audio(file):
         return None
 
 
+def show_setting_modal():
+    with st.expander("説明"):
+        tab1, tab2 = st.tabs(["アプリについて", "プライバシーガイドライン"])
+
+        with tab1:
+            readme_content = load_markdown_file("README.md")
+            st.markdown(readme_content)
+
+        with tab2:
+            privacy_content = load_markdown_file("privacy_guidelines.md")
+            st.markdown(privacy_content)
+
+
 def main():
     st.title("音声ファイルの文字起こしアプリ")
 
@@ -46,34 +62,31 @@ def main():
     if uploaded_audio_file is not None:
         st.session_state.uploaded_audio_file = uploaded_audio_file
 
-        # ファイルサイズのチェック（25MB以上の場合はエラーメッセージを表示）
         if uploaded_audio_file.size > 25 * 1024 * 1024:
             st.error("ファイルサイズが大きすぎます。25MB以下のファイルをアップロードしてください。")
         else:
-            # 音声を再生
             st.audio(uploaded_audio_file)
 
             transcription_message = st.empty()
             transcription_message.subheader("文字起こし中...")
 
-            # 文字起こし
             transcript = transcribe_audio(uploaded_audio_file)
 
-            # 文字起こし完了後、メッセージをクリアする
             transcription_message.empty()
 
             if transcript:
-                # 出力結果の表示
                 st.subheader("出力結果")
                 st.text_area("文字起こし文章", transcript, height=500)
 
-                # テキストファイルとしてダウンロード
                 st.download_button(
                     label="ダウンロード",
                     data=transcript.encode("utf-8"),
                     file_name="transcription.txt",
                     mime="text/plain"
                 )
+
+    if st.button("アプリの説明"):
+        show_setting_modal()
 
 
 if __name__ == "__main__":
